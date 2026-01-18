ddbe37fb50a468b0480e814a3367387110505605
Jz1221 <jzheng0512@gmail.com>
2026-01-18 18:17:51 +0800

    Axios. - Updated .gitignore to exclude .env.local files. - Refactored index.tsx and _layout.tsx for cleaner imports and structure. - Added types for User, Message, and Chat in a new types/index.ts file. - Updated dependencies in a bun.lock and package.json for compatibility.

---
+++ mobile/lib/axios.ts
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { useAuth } from "@clerk/clerk-expo";
+import { useCallback, useEffect } from "react";
+import * as Sentry from '@sentry/react-native';
+
+const API_URL = "http://192.168.1.13:3000/api" // example
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers:{
+    "Content-Type":"application/json",
+  },
+});
+
+export const useApi = () => {
+  const { getToken } = useAuth()
+  
+  useEffect(() => {
+    const requestInterceptor =  api.interceptors.request.use(async(config) => {
+      const token = await getToken();
+      
+      if(token) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
+
+      return config
+    })
+
+    const responseInterceptor = api.interceptors.response.use(
+      (response) => response, 
+      (error) => {
+      // log api error to sentry
+        if(error.response) {
+          Sentry.logger.error(
+            Sentry.logger.fmt`API request failed: ${error.config?.method?.toUpperCase()} ${error.config?.url}`,
+            {
+              status: error.response.status, 
+              endpoint: error.config?.url, 
+              method: error.config?.method 
+            }
+          )
+        } else if (error.request) {
+          Sentry.logger.warn("API request failed - no response", {
+            endpoint: error.config?.url,
+            method: error.config?.method,
+          });
+        }
+
+        return Promise.reject(error)
+    })
+
+    // cleanup: remove interceptors when components unmonts
+    return () => {
+      api.interceptors.request.eject(requestInterceptor);
+      api.interceptors.request.eject(responseInterceptor);
+    }
+  }, [getToken])
+
+  return api;
+}
