b9116b7620bd8140757111c41febdc1ebe05cba3
Jz1221 <jzheng0512@gmail.com>
2026-01-18 20:49:35 +0800

    feat: implements chat tab with loading and error states, add chat detail screen, and create chat item and empty UI components

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,14 +1,85 @@
-import { View, Text, ScrollView, Button } from "react-native";
-import * as Sentry from "@sentry/react-native"
+import ChatItem from "@/components/ChatItem";
+import EmptyUI from "@/components/EmptyUI";
+import { useChats } from "@/hooks/useChats";
+import { Chat } from "@/types";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { View, Text, ScrollView, ActivityIndicator, FlatList, Pressable } from "react-native";
+
 const ChatsTab = () => {
+  const router = useRouter();
+  const { data: chats, isLoading, error } = useChats();
+
+  if (isLoading) {
+    return (
+      <View className="flex-1 bg-surface items-center justify-center">
+        <ActivityIndicator size={"large"} color={"#f4A261"} />
+      </View>
+    );
+  }
+
+  if(error) {
+    return (
+      <View className="flex-1 bg-surface items-center justify-center">
+        <Text className="text-red-500">Failed to load chats</Text>
+      </View>
+    )
+  }
+
+  // todo: test this out later
+  const handleChatPress = (chat: Chat) => {
+    router.push({
+      pathname: "/chat/[id]",
+      params: {
+        id: chat._id,
+        participantId: chat.participant._id,
+        name: chat.participant.name,
+        avatar: chat.participant.avatar,
+      },
+    });
+  };
+
   return (
-    <ScrollView className="bg-surface flex-1"contentInsetAdjustmentBehavior="automatic">
-      <Text className="text-white">Chats Tab</Text>
-    </ScrollView>
+    <View className="flex-1 bg-surface">
+      <FlatList 
+      data={chats} 
+      keyExtractor={(item) => item._id}  
+      renderItem={({item}) => <ChatItem chat={item} onPress={() => handleChatPress(item)} /> } 
+      showsVerticalScrollIndicator={false}
+      contentInsetAdjustmentBehavior="automatic"
+      contentContainerStyle={{paddingHorizontal: 20, paddingTop: 16, paddingBottom: 24}}
+      ListHeaderComponent={<Header />}
+      ListEmptyComponent={<EmptyUI 
+          title="No chats yet"
+          subtitle="Start a conversation!"
+          iconName="chatbubbles-outline"
+          iconColor="#6B6B70"
+          iconSize={64}
+          buttonLabel="New Chat"
+          // todo: fix this later
+          onPressButton={() => console.log("pressed")}
+        />}
+      />
+    </View>
   );
 };
 
 export default ChatsTab;
+
+function Header() {
+  const router = useRouter();
+
+  return (
+    <View className="px-5 pt-2 pb-4">
+      <View className="flex-row items-center justify-between mt-4">
+        <Text className="text-2xl font-bold text-foreground">Chats</Text>
+        <Pressable className="size-10 bg-primary rounded-full items-center justify-center">
+          <Ionicons name="create-outline" size={20} color="#0D0D0F" />
+        </Pressable>
+      </View>
+    </View>
+  );
+}
 
 // TODO: provide this in video desc;
 // bun install nativewind react-native-reanimated@~3.17.4 react-native-safe-area-context@5.4.0
