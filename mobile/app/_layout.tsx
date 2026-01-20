d93e481456464d67b69356b94beb7aafbd29c24d
Jz1221 <jzheng0512@gmail.com>
2026-01-20 20:04:04 +0800

    feat: implement real-time chat features with socket.io integration, typing indicators and message handling

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -6,6 +6,7 @@ import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import AuthSync from "@/components/AuthSync";
 import { StatusBar } from "expo-status-bar";
 import * as Sentry from '@sentry/react-native';
+import SocketConnection from "@/components/SocketConnection";
 
 Sentry.init({
   dsn: 'https://d2568483b98bf64a1f7a97364072ad7e@o4510730710089728.ingest.us.sentry.io/4510730713956352',
@@ -33,6 +34,7 @@ export default Sentry.wrap(function RootLayout() {
     <ClerkProvider tokenCache={tokenCache}>
       <QueryClientProvider client={queryClient}>
         <AuthSync />
+        <SocketConnection />
         <StatusBar style="light"/>
         <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: "#0D0D0F" } }}>
           <Stack.Screen name="(auth)" options={{ animation: "fade"}} />
