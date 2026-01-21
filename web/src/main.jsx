44420ba72d0a5f5dcc9aa76fe191ea3757264238
Jz1221 <jzheng0512@gmail.com>
2026-01-21 10:43:09 +0800

    ChatPage components for user interaction. - Updated App component to handle route and authentication states. - Removed unused App.css and react.svg files - Added auth.png for illustrtion on the HomePage - Configured Axios for API requests.

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router"
+
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,10 +14,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>    
     </ClerkProvider>
   </StrictMode>
 );
